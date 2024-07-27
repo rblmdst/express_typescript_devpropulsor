78862bf28012a0929acbca685f08d65ae45a91a2
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2024-07-27 20:31:48 +0200

    [3-tier-architecture] Implementation of a 3-tier architecture

--- src/users/user.routes.ts
+++ src/users/user.routes.ts
@@ -1,53 +1,22 @@
-import { Request, Response, Router, json, urlencoded } from "express";
-import { users as userList } from "./users";
-let users = [...userList];
+import { Router, json, urlencoded } from "express";
+import { userControllerFactory } from "./user.controller";
+import { userRepositoryFactory } from "./user.repository";
+import { userServiceFactory } from "./user.service";
 
 export const userRouter = Router();
 const jsonParser = json();
 const bodyParser = urlencoded();
 
+// deps
+const userRepository = userRepositoryFactory();
+const userService = userServiceFactory(userRepository);
+const userController = userControllerFactory(userService);
+
 // localhost:3000/users?department=IT
-userRouter.get("/", (req: Request, res: Response) => {
-  const { department } = req.query;
-  if (department) {
-    return res.json(
-      users.filter(
-        (user) =>
-          user.department.toLowerCase() === (department as string).toLowerCase()
-      )
-    );
-  }
-  res.json(users);
-});
+userRouter.get("/", userController.getAllUsers);
 
-userRouter.get("/:userId", (req: Request, res: Response) => {
-  const { userId } = req.params;
-  const user = users.find((user) => user.id === userId);
-  if (user) {
-    return res.json(user);
-  }
-  res.status(404);
-  res.end();
-});
+userRouter.get("/:userId", userController.getUserById);
 
-userRouter.delete("/:userId", (req: Request, res: Response) => {
-  const { userId } = req.params;
-  const user = users.find((user) => user.id === userId);
-  if (!user) {
-    res.status(404);
-    return res.send();
-  }
+userRouter.delete("/:userId", userController.deteleUser);
 
-  users = users.filter((user) => user.id !== userId);
-  res.status(204);
-  res.end();
-});
-
-userRouter.post("/", bodyParser, jsonParser, (req: Request, res: Response) => {
-  const { department, name, level } = req.body;
-  const id = crypto.randomUUID();
-  const user = { id, department, name, level };
-  users = [...users, user];
-  res.status(201);
-  return res.json(user);
-});
+userRouter.post("/", bodyParser, jsonParser, userController.createUser);
