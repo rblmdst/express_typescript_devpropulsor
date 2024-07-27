8b5df6e789af99d958c44f7cd9147c0906b7e686
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2024-07-27 20:31:48 +0200

    [3-tier-architecture] Implementation of a 3-tier architecture

---
+++ src/users/user.controller.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+import { UserService } from "./user.service";
+import { createValidator } from "@rblmdst/scheval";
+
+export function userControllerFactory(userService: UserService) {
+  return {
+    getAllUsers: (req: Request, res: Response) => {
+      const { department } = req.query;
+      const users = userService.getUsers(department as string | undefined);
+      res.json(users);
+    },
+    getUserById: (req: Request, res: Response) => {
+      const { userId } = req.params;
+      const user = userService.getUser(userId);
+      if (user) {
+        return res.json(user);
+      }
+      res.status(404);
+      res.end();
+    },
+    deteleUser: (req: Request, res: Response) => {
+      const { userId } = req.params;
+      const user = userService.getUser(userId);
+      if (!user) {
+        res.status(404);
+        return res.send();
+      }
+      userService.deleteUser(userId);
+      res.status(204);
+      res.end();
+    },
+    createUser: (req: Request, res: Response) => {
+      const { department, name, level } = req.body;
+      const userData = { department, name, level };
+      const validatorConfig = {
+        department: {
+          type: ["string", "The user department must be a string"],
+          required: ["The user department is required"],
+          enum: [
+            ["IT", "Marketing", "HR"],
+            "The user department must be take one of the following values: 'IT', 'Marketing', 'HR'",
+          ],
+        },
+        name: {
+          type: ["string", "The user name must be a string"],
+          required: ["The user name is required"],
+        },
+        level: {
+          type: ["string", "The user department must be a string"],
+          required: ["The user department is required"],
+          enum: [
+            ["M", "J", "S"],
+            "The user department must be take one of the following values: 'M', 'J', 'S'",
+          ],
+        },
+      };
+      const validator = createValidator(validatorConfig);
+      const validationErrors = validator.validate(userData);
+      if (validationErrors.length) {
+        res.status(400);
+        return res.json(validationErrors);
+      }
+      const user = userService.createUser(userData);
+      res.status(201);
+      return res.json(user);
+    },
+  };
+}
