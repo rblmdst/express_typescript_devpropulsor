8fdf9442134c2e7ff35c833ff112c1ce3d244dc8
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2024-09-03 00:01:04 +0200

    [unit-test] Unit test layers of the 3-tier architecture

--- src/employees/employee.routes.ts
+++ src/employees/employee.routes.ts
@@ -2,13 +2,14 @@ import { Router, json, urlencoded } from "express";
 import { employeeControllerFactory } from "./employee.controller";
 import { employeeRepositoryFactory } from "./employee.repository";
 import { employeeServiceFactory } from "./employee.service";
+import { EmployeeModel } from "./employee.model";
 
 export const employeeRouter = Router();
 const jsonParser = json();
 const bodyParser = urlencoded();
 
 // deps
-const employeeRepository = employeeRepositoryFactory();
+const employeeRepository = employeeRepositoryFactory(EmployeeModel);
 const employeeService = employeeServiceFactory(employeeRepository);
 const employeeController = employeeControllerFactory(employeeService);
 
