948a7911e6a3ee9d6501d4ff9d782842a10af299
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2024-09-18 08:19:40 +0200

    [error-handling_gracefull-shutdown] Properly handle uncaught error/exception and Gracefully shutdown

--- src/core/middlewares/error.middleware.ts
+++ src/core/middlewares/error.middleware.ts
@@ -8,6 +8,16 @@ export const errorHandler = (
 ) => {
   console.log("Error Handler");
   console.error(err.stack);
+  if (err.name === "MongoServerError" && err.errorResponse.code === 11000) {
+    const { keyValue } = err.errorResponse;
+    //[ { field: "email", errors: ["err1"]}]
+    const errors = Object.entries(keyValue).map(([key, value]) => {
+      const message = `Duplicate constraint violation. The value '${value}' is already used!!`;
+      const fieldError = { field: key, errors: [message] };
+      return fieldError;
+    });
+    return res.status(400).json(errors);
+  }
   /* if(err instanceof Error){
     // do something
   } */
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import {
   createConfigServiceFactory,
 } from "./core/services/configuration.service";
 import { createApp } from "./core/app";
+import { Server } from "http";
+import mongoose from "mongoose";
 
+let server: Server;
 const main = async (configService: ConfigService) => {
   const PORT = configService.get("port") as number;
   const DB_URI = configService.get("dbUri") as string;
@@ -19,16 +22,61 @@ const main = async (configService: ConfigService) => {
 
   const app = createApp(configService);
 
-  app.listen(PORT, () => {
+  const server = app.listen(PORT, () => {
     console.log("Server started and listening on PORT ", PORT);
   });
+  return server;
 };
 
 const configService = createConfigServiceFactory();
 
 // boostrap
-main(configService).catch((err) => {
-  console.log("Error during bootstraping");
-  console.log(err);
-  process.exit(1);
+main(configService)
+  .then((serverInstance) => {
+    server = serverInstance;
+  })
+  .catch((err) => {
+    console.log("Error during bootstraping");
+    console.log(err);
+    process.exit(1);
+  });
+
+process.on("uncaughtException", (err) => {
+  console.log(`Uncaught Exception : ${err.message}`);
+  gracefulShutdown(server, 1);
 });
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log(`Uncaught rejection : ${promise} ${reason}`);
+  gracefulShutdown(server, 1);
+});
+
+process.on("SIGINT", () => {
+  console.log("Received SIGINT");
+  gracefulShutdown(server);
+});
+
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM");
+  gracefulShutdown(server);
+});
+
+function gracefulShutdown(server: Server, exitCode = 0) {
+  console.log("Graceful Shutdown... will close connections...");
+
+  server.close(() => {
+    console.log("Closed HTTP connections");
+    mongoose.connection.close(false).then(() => {
+      console.log("Connection to MongoDB closed");
+      console.log({ exitCode });
+      process.exit(exitCode || 0);
+    });
+  });
+
+  setTimeout(() => {
+    mongoose.connection.close(true).then(() => {
+      console.log("Connection to MongoDB force closed");
+      process.exit(1);
+    });
+  }, 2000).unref();
+}
